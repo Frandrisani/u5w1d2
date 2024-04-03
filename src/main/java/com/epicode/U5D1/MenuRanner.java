9651a48db91007fcb63a6c3f476bba04739852a6
Francesco Andrisani <fra.andrisani01@gmail.com>
2024-04-03 14:52:47 +0100

    implementazione della classe MenuRanner che estende CommandLineRunner

---
+++ src/main/java/com/epicode/U5D1/MenuRanner.java
@@ -0,0 +1,10 @@
+package com.epicode.U5D1;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class MenuRanner implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
