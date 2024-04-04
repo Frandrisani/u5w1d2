f6f91ac7e32e5e553777ea3e0585e791e67528ff
Francesco Andrisani <fra.andrisani01@gmail.com>
2024-04-04 16:47:42 +0100

    Test 4

--- src/test/java/com/epicode/U5D1/Esercizio30102023ApplicationTests.java
+++ src/test/java/com/epicode/U5D1/Esercizio30102023ApplicationTests.java
@@ -2,6 +2,9 @@ package com.epicode.U5D1;
 
 import com.epicode.U5D1.entities.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -11,7 +14,17 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @SpringBootTest
 class Esercizio30102023ApplicationTests {
 
-	// Test 1 --- Prezzo giusto dell'ordine
+	// Test 1 --- Controllo dell'ordine se è vuoto
+	@Test
+	void listItemVuoto() {
+		System.out.println("Test 3");
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(U5D1Application.class);
+		Tavolo t1 = (Tavolo) ctx.getBean("Tavolo1");
+		Ordine o1 = new Ordine(1, StatoOrdine.IN_CORSO, 1, t1);
+		assertEquals(0, o1.getItems().size());
+	}
+
+	// Test 2 --- Prezzo giusto dell'ordine
 	@Test
 	void PriceOrderTest1() {
 		System.out.println("Test 1");
@@ -29,7 +42,7 @@ class Esercizio30102023ApplicationTests {
 			ctx.close();
 	}
 
-	// Test 2 --- Controllo del giusot numero di prodotti nell'ordine
+	// Test 3 --- Controllo del giusot numero di prodotti nell'ordine
 	    @Test
 		void nuemeroProdottiGiusto() {
         System.out.println("Test 2");
@@ -41,7 +54,7 @@ class Esercizio30102023ApplicationTests {
 	}
 
 
-	// Test 3 --- Controllo del numero massimo di coperti per tavolo / verifichiamo che se inseriamo un numero di coperti superiore al massimo l'app lancia un'eccezione
+	// Test 4 --- Controllo del numero massimo di coperti per tavolo / verifichiamo che se inseriamo un numero di coperti superiore al massimo l'app lancia un'eccezione
 	@Test
 	void numeroCopertiMassimo() {
 		System.out.println("Test 3");
@@ -51,6 +64,13 @@ class Esercizio30102023ApplicationTests {
 			Ordine o1 = new Ordine(1, StatoOrdine.IN_CORSO, 6, t1);
 		});
 	}
+
+
+	// Test Parametrizzato - Set di dati di input (ad esempio, un elenco di ordini con diversi item e coperti) e verifica che il metodo getTotal() restituisca il prezzo corretto per ogni ordine.
+
+	@ParameterizedTest
+		@CsvSource("")
+	void PriceOrderTestParametrizzato(int idOrdine, StatoOrdine status, int covers, double total) {}
 
 
 
